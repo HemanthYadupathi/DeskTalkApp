f8b992fb73ab8716a6e6ef349bdcee6754740366
Hemanth B Y <hemanth9391@gmail.com>
2017-10-08 00:00:27 +0530

    Initial commit

---
+++ app/src/main/java/com/activity/desktalkapp/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.activity.desktalkapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Thread mSplashThread;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_splash);
+
+        final SplashActivity sPlashScreen = this;
+
+        // The thread to wait for splash screen events
+        mSplashThread =  new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized(this){
+                        // Wait given period of time or exit on touch
+                        wait(3000);
+                    }
+                }
+                catch(InterruptedException ex){
+                }
+
+                finish();
+
+                // Run next activity
+                Intent intent = new Intent();
+                intent.setClass(sPlashScreen, LoginActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        mSplashThread.start();
+    }
+}
