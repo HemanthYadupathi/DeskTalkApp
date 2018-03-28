1efb2a520b4a3568c7e75cdcbaf2bd426ff64de2
pavank814 <32876898+pavank814@users.noreply.github.com>
2018-03-28 15:45:03 +0530

    --Added firebase on registration on Launch of Application

---
+++ app/src/main/java/com/desktalk/util/Config.java
@@ -0,0 +1,24 @@
+package com.desktalk.util;
+
+/**
+ * Created by Ravi Tamada on 28/09/16.
+ * www.androidhive.info
+ */
+
+public class Config {
+
+    // global topic to receive app wide push notifications
+    public static final String TOPIC_GLOBAL = "global";
+    public static String ID = " ";
+//    private final String ID = " ";
+
+    // broadcast receiver intent filters
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String PUSH_NOTIFICATION = "pushNotification";
+
+    // id to handle the notification in the notification tray
+    public static int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+
+    public static final String SHARED_PREF = "ah_firebase";
+}
---
+++ app/src/main/java/com/desktalk/util/MyFirebaseMessagingService.java
@@ -0,0 +1,17 @@
+package com.desktalk.util;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Pavan.Chunchula on 22-03-2018.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
+
+}
